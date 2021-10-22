0bcbb4a3afe04ccacdf9c100e6be5332d6ce4ba3
Joseph <joseph.coderose@gmail.com>
2021-10-22 17:30:22 -0300

    Auth config in laravel and node lib

--- images/php/run.sh
+++ images/php/run.sh
@@ -3,12 +3,20 @@
 #else
 #    composer create-project laravel/laravel /var/www/html/$APPPATH;
 #fi
-
+#
 #fixed to test, I don't know why the above code doesn't work --'
 composer create-project laravel/laravel /var/www/html/$APPPATH;
+#
+#if [$HAS_AUTHENTICATION == true]; then
+composer require laravel/breeze --dev
+php artisan breeze:install
+npm install
+npm run dev
+#fi
+#
+php artisan migrate
 chmod 777 /var/www/html/$APPPATH/storage -R;
-
+#
 #/var/www/html/$APPPATH/artisan migrate --force;
 #/var/www/html/$APPPATH/artisan db:seed --force;
 #cp /var/www/html/$APPPATH/.env.example /var/www/html/$APPPATH/.env;
-
